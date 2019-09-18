c3e72b7ef7e5140718090295baf8d866f217ab17
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-09-18 09:51:01 -0300

    correção dos links e adição do screenshot

--- wp-content/themes/templateWordpressCurso/header.php
+++ wp-content/themes/templateWordpressCurso/header.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html <?php language_attributes()?>>
 <head>
 	<!-- Meta tag obrigatória -->
-	<meta charset="UTF-8">
+	<meta <?php bloginfo('charset')?>>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="author" content="Yasmim Morais de Vasconcelos">
-	<title>AEBA - Associação dos Empresários do Bairro do Alecrim</title>
+	<title><?php bloginfo('title')?></title>
+
 	<!-- Folha de estilo -->
-	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_url')?>/assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>">
+
 	<!-- Script.js -->
-	<script type="text/javascript" src="assets/js/script.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url')?>/assets/js/script.js"></script>
 	
 	<!-- Favicon  -->
-	<link rel="shortcut icon" href="assets/img/favicon_aeba.png">
+	<link rel="shortcut icon" href="<?php bloginfo('template_url')?>/assets/img/favicon_aeba.png">
 	
 	<!-- Links das fontes: Roboto e Open Sans -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
 	
 	<!-- Link Font Awesome - ícones -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<?php wp_head()?>
 </head>
 <body>
 	<!-- Ícone do whatsapp fixo -->
@@ -31,7 +35,7 @@
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
 
-		  <a class="navbar-brand ml-5" href="#"><img src="assets/img/logo-menu.jpg" style="width: 120px;" alt=""></a>
+		  <a class="navbar-brand ml-5" href="#"><img src="<?php bloginfo('template_url');?>/assets/img/logo-menu.jpg" style="width: 120px;" alt=""></a>
 
 		  <div class="collapse navbar-collapse m-3" id="navbarTogglerDemo03">
 		    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mx-auto">
